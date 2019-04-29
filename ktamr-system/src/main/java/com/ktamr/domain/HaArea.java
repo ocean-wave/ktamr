d70b0ce5a61e905d0365b705b2304206e07d0ba8
961504963 <961504963@qq.com>
2019-04-29 16:13:54 +0800

    1

--- ktamr-system/src/main/java/com/ktamr/domain/HaArea.java
+++ ktamr-system/src/main/java/com/ktamr/domain/HaArea.java
@@ -10,250 +10,287 @@ import java.util.List;
 
 public class HaArea extends BaseEntity {
 
-  private Integer areaId;
-  private String name;
-  private String descripTion;
-  private Date lastcheckTime;
-  private String state;
-  private String reserved;
-  private String areaNo;
-  private String shortName;
-  private String addr;
-  private String sumDay;
-  @JsonFormat( pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
-  @DateTimeFormat( pattern = "yyyy-MM-dd HH:mm:ss")
-  private Date lastsumDay;
-  private String ds;
-  private Date createTime;
-  private Date modifyTime;
-  private Date auditDay;
-  private String auditResult;
-  private String registeredNo;
+    private Integer areaId;
+    private String name;
+    private String descripTion;
+    private Date lastcheckTime;
+    private String state;
+    private String reserved;
+    private String areaNo;
+    private String shortName;
+    private String addr;
+    private String sumDay;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date lastsumDay;
+    private String ds;
+    private Date createTime;
+    private Date modifyTime;
+    private Date auditDay;
+    private String auditResult;
+    private String registeredNo;
+
+    private String monthType;
+    private Integer haAreaCount;
+    private String rgn;
+    private String rgnIds;
 
 
-  //开始存
-  private HaMeter haMeter;
-  private HaRgn haRgn;
-  private HavMeterinfo havMeterinfo;
-  private HaBuilding haBuilding;
+    //开始存
+    private HaMeter haMeter;
+    private HaRgn haRgn;
+    private HavMeterinfo havMeterinfo;
+    private HaBuilding haBuilding;
 
-  //定义接收
-  private  Integer atnNumber;//不良表数
-  private Integer countAreaNo;//2级菜单多少子的小区名称记录数
-  private String cmdName;//定义别名类型接收
-  private String keyWord; //ssss
-  //方便用areaid中in的范围查询
-  List<Integer> idsList;
+    //定义接收
+    private Integer atnNumber;//不良表数
+    private Integer countAreaNo;//2级菜单多少子的小区名称记录数
+    private String cmdName;//定义别名类型接收
+    private String keyWord; //ssss
+    //方便用areaid中in的范围查询
+    List<Integer> idsList;
 
-  public Integer getAreaId() {
-    return areaId;
-  }
+    public Integer getAreaId() {
+        return areaId;
+    }
 
-  public void setAreaId(Integer areaId) {
-    this.areaId = areaId;
-  }
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public String getDescripTion() {
-    return descripTion;
-  }
+    public String getDescripTion() {
+        return descripTion;
+    }
 
-  public void setDescripTion(String descripTion) {
-    this.descripTion = descripTion;
-  }
+    public void setDescripTion(String descripTion) {
+        this.descripTion = descripTion;
+    }
 
-  public Date getLastcheckTime() {
-    return lastcheckTime;
-  }
+    public Date getLastcheckTime() {
+        return lastcheckTime;
+    }
 
-  public void setLastcheckTime(Date lastcheckTime) {
-    this.lastcheckTime = lastcheckTime;
-  }
+    public void setLastcheckTime(Date lastcheckTime) {
+        this.lastcheckTime = lastcheckTime;
+    }
 
-  public String getState() {
-    return state;
-  }
+    public String getState() {
+        return state;
+    }
 
-  public void setState(String state) {
-    this.state = state;
-  }
+    public void setState(String state) {
+        this.state = state;
+    }
 
-  public String getReserved() {
-    return reserved;
-  }
+    public String getReserved() {
+        return reserved;
+    }
 
-  public void setReserved(String reserved) {
-    this.reserved = reserved;
-  }
+    public void setReserved(String reserved) {
+        this.reserved = reserved;
+    }
 
-  public String getAreaNo() {
-    return areaNo;
-  }
+    public String getAreaNo() {
+        return areaNo;
+    }
 
-  public void setAreaNo(String areaNo) {
-    this.areaNo = areaNo;
-  }
+    public void setAreaNo(String areaNo) {
+        this.areaNo = areaNo;
+    }
 
-  public String getShortName() {
-    return shortName;
-  }
+    public String getShortName() {
+        return shortName;
+    }
 
-  public void setShortName(String shortName) {
-    this.shortName = shortName;
-  }
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 
-  public String getAddr() {
-    return addr;
-  }
+    public String getAddr() {
+        return addr;
+    }
 
-  public void setAddr(String addr) {
-    this.addr = addr;
-  }
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 
-  public String getSumDay() {
-    return sumDay;
-  }
+    public String getSumDay() {
+        return sumDay;
+    }
 
-  public void setSumDay(String sumDay) {
-    this.sumDay = sumDay;
-  }
+    public void setSumDay(String sumDay) {
+        this.sumDay = sumDay;
+    }
 
-  public Date getLastsumDay() {
-    return lastsumDay;
-  }
+    public Date getLastsumDay() {
+        return lastsumDay;
+    }
 
-  public void setLastsumDay(Date lastsumDay) {
-    this.lastsumDay = lastsumDay;
-  }
+    public void setLastsumDay(Date lastsumDay) {
+        this.lastsumDay = lastsumDay;
+    }
 
-  public String getDs() {
-    return ds;
-  }
+    public String getDs() {
+        return ds;
+    }
 
-  public void setDs(String ds) {
-    this.ds = ds;
-  }
+    public void setDs(String ds) {
+        this.ds = ds;
+    }
 
-  @Override
-  public Date getCreateTime() {
-    return createTime;
-  }
+    @Override
+    public Date getCreateTime() {
+        return createTime;
+    }
 
-  @Override
-  public void setCreateTime(Date createTime) {
-    this.createTime = createTime;
-  }
+    @Override
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
-  @Override
-  public Date getModifyTime() {
-    return modifyTime;
-  }
+    @Override
+    public Date getModifyTime() {
+        return modifyTime;
+    }
 
-  @Override
-  public void setModifyTime(Date modifyTime) {
-    this.modifyTime = modifyTime;
-  }
+    @Override
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
 
-  public Date getAuditDay() {
-    return auditDay;
-  }
+    public Date getAuditDay() {
+        return auditDay;
+    }
 
-  public void setAuditDay(Date auditDay) {
-    this.auditDay = auditDay;
-  }
+    public void setAuditDay(Date auditDay) {
+        this.auditDay = auditDay;
+    }
 
-  public String getAuditResult() {
-    return auditResult;
-  }
+    public String getAuditResult() {
+        return auditResult;
+    }
 
-  public void setAuditResult(String auditResult) {
-    this.auditResult = auditResult;
-  }
+    public void setAuditResult(String auditResult) {
+        this.auditResult = auditResult;
+    }
 
-  public String getRegisteredNo() {
-    return registeredNo;
-  }
+    public String getRegisteredNo() {
+        return registeredNo;
+    }
 
-  public void setRegisteredNo(String registeredNo) {
-    this.registeredNo = registeredNo;
-  }
+    public void setRegisteredNo(String registeredNo) {
+        this.registeredNo = registeredNo;
+    }
 
-  public HaMeter getHaMeter() {
-    return haMeter;
-  }
+    public HaMeter getHaMeter() {
+        return haMeter;
+    }
 
-  public void setHaMeter(HaMeter haMeter) {
-    this.haMeter = haMeter;
-  }
+    public void setHaMeter(HaMeter haMeter) {
+        this.haMeter = haMeter;
+    }
 
-  public HaRgn getHaRgn() {
-    return haRgn;
-  }
+    public HaRgn getHaRgn() {
+        return haRgn;
+    }
 
-  public void setHaRgn(HaRgn haRgn) {
-    this.haRgn = haRgn;
-  }
+    public void setHaRgn(HaRgn haRgn) {
+        this.haRgn = haRgn;
+    }
 
-  public HavMeterinfo getHavMeterinfo() {
-    return havMeterinfo;
-  }
+    public HavMeterinfo getHavMeterinfo() {
+        return havMeterinfo;
+    }
 
-  public void setHavMeterinfo(HavMeterinfo havMeterinfo) {
-    this.havMeterinfo = havMeterinfo;
-  }
+    public void setHavMeterinfo(HavMeterinfo havMeterinfo) {
+        this.havMeterinfo = havMeterinfo;
+    }
 
-  public HaBuilding getHaBuilding() {
-    return haBuilding;
-  }
+    public HaBuilding getHaBuilding() {
+        return haBuilding;
+    }
 
-  public void setHaBuilding(HaBuilding haBuilding) {
-    this.haBuilding = haBuilding;
-  }
+    public void setHaBuilding(HaBuilding haBuilding) {
+        this.haBuilding = haBuilding;
+    }
 
-  public Integer getAtnNumber() {
-    return atnNumber;
-  }
+    public Integer getAtnNumber() {
+        return atnNumber;
+    }
 
-  public void setAtnNumber(Integer atnNumber) {
-    this.atnNumber = atnNumber;
-  }
+    public void setAtnNumber(Integer atnNumber) {
+        this.atnNumber = atnNumber;
+    }
 
-  public Integer getCountAreaNo() {
-    return countAreaNo;
-  }
+    public Integer getCountAreaNo() {
+        return countAreaNo;
+    }
 
-  public void setCountAreaNo(Integer countAreaNo) {
-    this.countAreaNo = countAreaNo;
-  }
+    public void setCountAreaNo(Integer countAreaNo) {
+        this.countAreaNo = countAreaNo;
+    }
 
-  public String getCmdName() {
-    return cmdName;
-  }
+    public String getCmdName() {
+        return cmdName;
+    }
 
-  public void setCmdName(String cmdName) {
-    this.cmdName = cmdName;
-  }
+    public void setCmdName(String cmdName) {
+        this.cmdName = cmdName;
+    }
 
-  public String getKeyWord() {
-    return keyWord;
-  }
+    public String getKeyWord() {
+        return keyWord;
+    }
 
-  public void setKeyWord(String keyWord) {
-    this.keyWord = keyWord;
-  }
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
 
-  public List<Integer> getIdsList() {
-    return idsList;
-  }
+    public List<Integer> getIdsList() {
+        return idsList;
+    }
 
-  public void setIdsList(List<Integer> idsList) {
-    this.idsList = idsList;
-  }
+    public void setIdsList(List<Integer> idsList) {
+        this.idsList = idsList;
+    }
+
+    public String getMonthType() {
+        return monthType;
+    }
+
+    public void setMonthType(String monthType) {
+        this.monthType = monthType;
+    }
+
+    public Integer getHaAreaCount() {
+        return haAreaCount;
+    }
+
+    public void setHaAreaCount(Integer haAreaCount) {
+        this.haAreaCount = haAreaCount;
+    }
+
+    public String getRgn() {
+        return rgn;
+    }
+
+    public void setRgn(String rgn) {
+        this.rgn = rgn;
+    }
+
+    public String getRgnIds() {
+        return rgnIds;
+    }
+
+    public void setRgnIds(String rgnIds) {
+        this.rgnIds = rgnIds;
+    }
 }
