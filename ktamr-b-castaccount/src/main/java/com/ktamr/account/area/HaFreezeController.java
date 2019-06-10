39f07751435c9e5112c986517572fa5331c7cd2b
2646613694 <2646613694@qq.com>
2019-06-10 13:28:56 +0800

    小可爱6月10上传提交

--- ktamr-b-castaccount/src/main/java/com/ktamr/account/area/HaFreezeController.java
+++ ktamr-b-castaccount/src/main/java/com/ktamr/account/area/HaFreezeController.java
@@ -23,15 +23,16 @@ public class HaFreezeController {
 
     /**
      * 点击小区结算的功能按钮
+     *
      * @param haFreeze
      * @return
      */
     @RequestMapping("/bill_op")
     @ResponseBody
-    public String bill_op(HaFreeze haFreeze, HaArea haArea, HttpSession session){
+    public String bill_op(HaFreeze haFreeze, HaArea haArea, HttpSession session) {
 
         //判断是否点击费用结算按钮的事件
-        if(haFreeze.getOpType().equals("费用结算")){
+        if (haFreeze.getOpType().equals("费用结算")) {
             Integer insertHaFreeze1 = haFreezeService.insertHaFreeze11(haFreeze);
             Integer insertHaFreeze2 = haFreezeService.insertHaFreeze12(haFreeze);
             Integer binsertHaFreeze2 = haFreezeService.BinsertHaFreeze2(haFreeze);
@@ -41,12 +42,12 @@ public class HaFreezeController {
             Integer binsertHaFreeze43 = haFreezeService.BinsertHaFreeze43(haFreeze);
             Integer binsertHaFreeze51 = haFreezeService.BinsertHaFreeze51(haFreeze);
             Integer binsertHaFreeze52 = haFreezeService.BinsertHaFreeze52(haFreeze);
-            if((insertHaFreeze1+insertHaFreeze2+binsertHaFreeze2+binsertHaFreeze3+binsertHaFreeze41+binsertHaFreeze42
-            +binsertHaFreeze43+binsertHaFreeze51+binsertHaFreeze52
-            )>0){
+            if ((insertHaFreeze1 + insertHaFreeze2 + binsertHaFreeze2 + binsertHaFreeze3 + binsertHaFreeze41 + binsertHaFreeze42
+                    + binsertHaFreeze43 + binsertHaFreeze51 + binsertHaFreeze52
+            ) > 0) {
                 return "<font color='green'>费用结算成功</font>";
             }
-        }else if (haFreeze.getOpType().equals("完成结算")){
+        } else if (haFreeze.getOpType().equals("完成结算")) {
             Object attribute = session.getAttribute("operatorCode");
             Integer integer1 = haAreaService.updateWanChengJieSuanOne(haArea);
             haArea.setOpertorCode(String.valueOf(attribute));
@@ -56,7 +57,7 @@ public class HaFreezeController {
             Integer integer5 = haAreaService.updateWanChenJieSuanFive(haArea);
             Integer integer6 = haAreaService.updateWanChenJieSuanSix(haArea);
             Integer integer7 = haAreaService.updateWanChenJieSuanSeven(haArea);
-            if((integer1+integer2+integer3+integer4+integer5+integer6+integer7)>0){
+            if ((integer1 + integer2 + integer3 + integer4 + integer5 + integer6 + integer7) > 0) {
                 return "<font color='green'>完成结算成功</font>";
             }
 
@@ -64,7 +65,6 @@ public class HaFreezeController {
         }
         return "<font color='red'>操作失败</font>";
     }
-
 
 
 }
