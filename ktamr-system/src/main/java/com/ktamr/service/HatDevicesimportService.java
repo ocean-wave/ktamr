012aa032102172cab906930cca1c14190402c1ee
961504963 <961504963@qq.com>
2019-06-27 11:31:08 +0800

    9:3

--- ktamr-system/src/main/java/com/ktamr/service/HatDevicesimportService.java
+++ ktamr-system/src/main/java/com/ktamr/service/HatDevicesimportService.java
@@ -9,9 +9,57 @@ public interface HatDevicesimportService {
 
     List<HatDevicesimport> HatDevicesimportList(HatDevicesimport hatDevicesimport);
 
+    //新增导入设备(待校验)
     Integer addHatDevicesimport(HatDevicesimport hatDevicesimport);
 
+    //修改设备档案
     Integer updateHatDevicesimport(HatDevicesimport hatDevicesimport);
 
+    //清理零时表
     Integer deleteHatDevicesimport(HatDevicesimport hatDevicesimport);
+
+    //所属小区校验
+    Integer belongAreaCheck(HatDevicesimport hatDevicesimport);
+
+    //连接说明校验
+    Integer joinSayCheck(HatDevicesimport hatDevicesimport);
+
+    //设备名称校验
+    Integer centorNameCheck(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验1
+    Integer centorAddrCheck1(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验2
+    Integer centorAddrCheck2(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验3
+    Integer centorAddrCheck3(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验4
+    Integer centorAddrCheck4(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验5
+    Integer centorAddrCheck5(HatDevicesimport hatDevicesimport);
+
+    //设备地址校验6
+    Integer centorAddrCheck6(HatDevicesimport hatDevicesimport);
+
+    //判断是新增还是修改
+    Integer checkAddOrUpdate(HatDevicesimport hatDevicesimport);
+
+    //补齐默认数值
+    Integer completeDefaultValues(HatDevicesimport hatDevicesimport);
+
+    //查询总条目
+    Integer queryAllHatDevicesimportCount(HatDevicesimport hatDevicesimport);
+
+    //查询新增数
+    Integer addsCount(HatDevicesimport hatDevicesimport);
+
+    //查询修改数
+    Integer updsCount(HatDevicesimport hatDevicesimport);
+
+    //新增设备档案——新增集中器档案和手抄线路档案
+    Integer addCentorsAndHandDevices(HatDevicesimport hatDevicesimport);
 }
--- ktamr-system/src/main/java/com/ktamr/service/impl/HatDevicesimportServiceImpl.java
+++ ktamr-system/src/main/java/com/ktamr/service/impl/HatDevicesimportServiceImpl.java
@@ -29,4 +29,79 @@ public class HatDevicesimportServiceImpl implements HatDevicesimportService {
     public Integer deleteHatDevicesimport(HatDevicesimport hatDevicesimport) {
         return hatDevicesimportMapper.deleteHatDevicesimport(hatDevicesimport);
     }
+
+    @Override
+    public Integer belongAreaCheck(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.belongAreaCheck(hatDevicesimport);
+    }
+
+    @Override
+    public Integer joinSayCheck(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.joinSayCheck(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorNameCheck(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorNameCheck(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck1(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck1(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck2(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck2(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck3(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck3(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck4(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck4(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck5(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck5(hatDevicesimport);
+    }
+
+    @Override
+    public Integer centorAddrCheck6(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.centorAddrCheck6(hatDevicesimport);
+    }
+
+    @Override
+    public Integer checkAddOrUpdate(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.checkAddOrUpdate(hatDevicesimport);
+    }
+
+    @Override
+    public Integer completeDefaultValues(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.completeDefaultValues(hatDevicesimport);
+    }
+
+    @Override
+    public Integer queryAllHatDevicesimportCount(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.queryAllHatDevicesimportCount(hatDevicesimport);
+    }
+
+    @Override
+    public Integer addsCount(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.addsCount(hatDevicesimport);
+    }
+
+    @Override
+    public Integer updsCount(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.updsCount(hatDevicesimport);
+    }
+
+    @Override
+    public Integer addCentorsAndHandDevices(HatDevicesimport hatDevicesimport) {
+        return hatDevicesimportMapper.addCentorsAndHandDevices(hatDevicesimport);
+    }
 }
